ca43a01f40503899b5462f17fa74bfc41932106e
Eddie <tyh1030@gmail.com>
2025-04-07 16:58:04 +0800

    vault backup: 2025-04-07 16:58:04

--- 3.R/energy_analysis.R
+++ 3.R/energy_analysis.R
@@ -21,13 +21,14 @@ con <- dbConnect(
   password = config$postgres$password
 )
 ds_w_raw_data <- dbGetQuery(con, "select * from iotrawdata where instr_model='W349'")
-ds_w_hourly_data <- dbGetQuery(con, "select * from iotrawdata where instr_model='W349'")
-s_w_daily_data <- dbGetQuery(con, "select * from iotrawdata where instr_model='W349'")
-s_w_monthly_data <- dbGetQuery(con, "select * from iotrawdata where instr_model='W349'")
+ds_w_hourly_data <- dbGetQuery(con, "select * from vw_houly_kw_used")
+ds_w_daily_data <- dbGetQuery(con, "select * from vw_daily_kw_used")
+ds_w_monthly_data <- dbGetQuery(con, "select * from vw_monthly_kw_used")
 # Disconnect
 dbDisconnect(con)
 
 # Compute statistics
+# example of summary statistics--01
 summary_w_raw_data <- ds_w_raw_data %>%
   group_by(asset_id) %>%
   summarise(
@@ -43,11 +44,10 @@ summary_w_raw_data <- ds_w_raw_data %>%
     cv=sd(rawvalue) / mean(rawvalue),
   )
 
-print(summary_w_raw_data)
+# print(summary_w_raw_data)
 # Visualizing with ggplot2
 
-
-ggplot(summary_stats_V, aes(x = asset_id, y = mean)) +
+ggplot(summary_w_raw_data, aes(x = asset_id, y = mean)) +
   geom_col(fill = "lightblue") +
   geom_errorbar(aes(ymin = mean - std_error, ymax = mean + std_error), width = 0.2) +
   geom_point(aes(y = median), color = "red", size = 3) + 
@@ -56,5 +56,64 @@ ggplot(summary_stats_V, aes(x = asset_id, y = mean)) +
        x = "Asset type") +
   theme_minimal()
 
+# example of summary statistics--02
+summary_w_hourly_data <- ds_w_hourly_data %>%
+  group_by(asset_id) %>%
+  summarise(
+    range= max(kw_used) - min(kw_used),
+    max= max(kw_used),
+    min= min(kw_used),
+    counts = n(),
+    mean = mean(kw_used),
+    median = median(kw_used),
+    Q1=quantile(kw_used, probs = 0.25),
+    Q2=quantile(kw_used, probs = 0.5),
+    Q3=quantile(kw_used, probs = 0.75),
+    variance=var(kw_used),
+    std_error = sd(kw_used) / sqrt(n()),
+    cv=sd(kw_used) / mean(kw_used),
+  )
+# print(summary_w_hourly_data)
+# Visualizing with ggplot2
+ggplot(ds_w_hourly_data, aes(x = date_hour, y = kw_used)) +
+  geom_col(fill = "lightblue") +
+  geom_point(aes(y = kw_used), color = "blue", size = 1) + 
+  labs(title = "KW used by Hour",
+       y = "kw",
+       x = "Time") +
+  theme_minimal()
+# example of summary statistics--03
+summary_w_daily_data <- ds_w_daily_data %>%
+  group_by(weekday) %>%
+  summarise(
+    range= max(kw_used) - min(kw_used),
+    max= max(kw_used),
+    min= min(kw_used),
+    counts = n(),
+    mean = mean(kw_used),
+    median = median(kw_used),
+    Q1=quantile(kw_used, probs = 0.25),
+    Q2=quantile(kw_used, probs = 0.5),
+    Q3=quantile(kw_used, probs = 0.75),
+    variance=var(kw_used),
+    std_error = sd(kw_used) / sqrt(n()),
+    cv=sd(kw_used) / mean(kw_used),
+  )
+ggplot(summary_w_daily_data, aes(x = weekday, y = mean)) +
+  geom_col(fill = "lightblue") +
+  geom_errorbar(aes(ymin = mean - 2*std_error, ymax = mean + 2*std_error), width = 0.2) +
+  geom_point(aes(y = median), color = "red", size = 3) + 
+  labs(title = "Mean, Median (Red Dots) and Standard Error",
+       y = "kw",
+       x = "weekday per week") +
+  theme_minimal()
+## 2
+ggplot(ds_w_daily_data, aes(x = date_dd, y = kw_used)) +
+  geom_col(fill = "lightblue") +
+  geom_point(aes(y = kw_used), color = "blue", size = 1) + 
+  labs(title = "KW used by days",
+       y = "kw",
+       x = "Time") +
+  theme_minimal()
 
 
